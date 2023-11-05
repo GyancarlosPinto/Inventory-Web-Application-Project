30780bb63eb28c87a06900a91ea6c40aa1d20c30
GyancarlosPinto <gyancarlospinto@pursuit.org>
2023-11-05 16:25:58 -0500

    Added createProduct Function, Updated HTML form

--- app_events/apps.js
+++ app_events/apps.js
@@ -1,11 +1,13 @@
 const productsList = [
     {
-        productName: "Dracula Beer Keg Dispenser",
+        name: "Dracula Beer Keg Dispenser",
         brandName: "Fangsylvania Brewing Company",
         productType: "Kegerator",
+        img: "https://images.nightcafe.studio/jobs/0dcuv9GCYC8TXqwlaJEG/0dcuv9GCYC8TXqwlaJEG--1--nqej0.jpg?tr=w-1600,c-at_max",
+        description: "",
         price: 1392.00,
         inStock: 79,
-        season: "Autumn"
+        seasons: "Autumn"
     }
 ]
 
@@ -13,11 +15,71 @@ function createProduct(product) {
     const productContainer = document.createElement("div");
     productContainer.classList.add("product-card");
 
-    const productImageElement = document.createElement("img");
-    productImageElement.setAttribute("src", product.img);
-
     const productName = document.createElement("strong");
     productName.textContent = product.name;
+
+    const productBrandName = document.createElement("em");
+    productBrandName.textContent = product.brandName;
+
+    const productType = document.createElement("p");
+    productType.textContent = product.productType;
+
+    const productImageUrl = document.createElement("img");
+    productImageUrl.setAttribute("src", product.img);
+
+    const productDescription = document.createElement("p");
+    productDescription.textContent = product.description;
+
+    const productPrice = document.createElement("p");
+    productPrice.textContent = product.price;
+
+    const productInStock = document.createElement("p");
+    productInStock.textContent = product.inStock;
+
+    const seasons = document.createElement("p");
+    seasons.textContent = product.seasons;
+
+    productContainer.append(
+        productImageUrl,
+        productName,
+        productBrandName,
+        productType,
+        productDescription,
+        productPrice,
+        productInStock,
+        seasons,
+    )
+
+    const main = document.querySelector("main");
+    main.append(productContainer)
 }
 
-createProduct();
+const formElement = document.querySelector("#new-product-form");
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const name = formElement.productName.value;
+    const brandName = formElement.productBrandName.value;
+    const productType = formElement.productType.value;
+    const img = formElement.productImageUrl.value;
+    const description = formElement.productDescription.value;
+    const price = formElement.productPrice.value;
+    const inStock = formElement.productInStock.value;
+    const seasons = formElement.seasons.value;
+
+    createProduct({
+        name,
+        brandName,
+        productType,
+        img,
+        description,
+        price,
+        inStock,
+        seasons
+    })
+})
+
+for (const product of productsList) {
+    createProduct(product);
+}
+
