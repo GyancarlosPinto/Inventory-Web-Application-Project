06016b0ca2f1b7ad7298f3d7a251563faff39a6e
GyancarlosPinto <gyancarlospinto@pursuit.org>
2023-11-05 14:08:25 -0500

    Updated HTML Product Form

--- app_events/apps.js
+++ app_events/apps.js
@@ -1,9 +1,9 @@
 const productsList = [
     {
-        productName: "Dracula Beer Keg Dispensary",
+        productName: "Dracula Beer Keg Dispenser",
         brandName: "Fangsylvania Brewing Company",
         productType: "Kegerator",
-        price: 392.00,
+        price: 1392.00,
         inStock: 79,
         season: "Autumn"
     }
@@ -13,6 +13,11 @@ function createProduct(product) {
     const productContainer = document.createElement("div");
     productContainer.classList.add("product-card");
 
+    const productImageElement = document.createElement("img");
+    productImageElement.setAttribute("src", product.img);
+
     const productName = document.createElement("strong");
     productName.textContent = product.name;
 }
+
+createProduct();
